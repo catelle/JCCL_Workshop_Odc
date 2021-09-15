d5530971e1b80d90f190b2ed375a5dd17f104e91
junior temgoua <juniortemgoua0@gmail.com>
2021-09-15 15:59:05 +0100

    start create models

---
+++ app/src/main/java/com/example/jccl_network_project/models/Publication.java
@@ -0,0 +1,12 @@
+package com.example.jccl_network_project.models;
+
+public class Publication {
+    private String publication_id;
+    private String categorie;
+    private String description;
+    private String destination;
+    private int duree;
+    private String emplacement_piece ;
+    private String lien;
+    private String publication_id;
+}
