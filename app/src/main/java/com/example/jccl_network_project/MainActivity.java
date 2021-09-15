b7262bbd158701c51bd57cf90df240706bcb0d5e
junior temgoua <juniortemgoua0@gmail.com>
2021-09-15 09:42:14 +0100

    ajout des tabs sur le profil

--- app/src/main/java/com/example/jccl_network_project/MainActivity.java
+++ app/src/main/java/com/example/jccl_network_project/MainActivity.java
@@ -4,25 +4,46 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentContainer;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TableLayout;
 
+import com.example.jccl_network_project.adapters.Tab_fragment_Adapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigation;
-
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
+        tabLayout =  findViewById(R.id.tabLayout);
+        viewPager2 = findViewById(R.id.viewpager2);
+
         bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.setOnNavigationItemSelectedListener( navigationItemSelectedListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container , new Home_fragment()).commit();
+
+        FragmentManager fm = getSupportFragmentManager();
+        Tab_fragment_Adapter adapter = new Tab_fragment_Adapter(fm , getLifecycle());
+        viewPager2.setAdapter(adapter);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Historique"));
+        tabLayout.addTab(tabLayout.newTab().setText("Favoris"));
+        tabLayout.addTab(tabLayout.newTab().setText("Abonnees"));
+        tabLayout.addTab(tabLayout.newTab().setText("A propos"));
+        tabLayout.addOnTabSelectedListener(tabSelectedListener);
+        viewPager2.registerOnPageChangeCallback(pageChangeCallback);
+
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -50,4 +71,28 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 }
             };
+
+    private TabLayout.OnTabSelectedListener tabSelectedListener = new TabLayout.OnTabSelectedListener() {
+        @Override
+        public void onTabSelected(TabLayout.Tab tab) {
+            viewPager2.setCurrentItem(tab.getPosition());
+        }
+
+        @Override
+        public void onTabUnselected(TabLayout.Tab tab) {
+
+        }
+
+        @Override
+        public void onTabReselected(TabLayout.Tab tab) {
+
+        }
+    };
+
+    private ViewPager2.OnPageChangeCallback pageChangeCallback = new ViewPager2.OnPageChangeCallback() {
+        @Override
+        public void onPageSelected(int position) {
+            tabLayout.selectTab(tabLayout.getTabAt(position));
+        }
+    };
 }
--- app/src/main/java/com/example/jccl_network_project/tab_home_page/A_propos.java
+++ app/src/main/java/com/example/jccl_network_project/tab_home_page/A_propos.java
@@ -1,36 +1,32 @@
-package com.example.jccl_network_project;
+package com.example.jccl_network_project.tab_home_page;
 
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TableLayout;
+
+import com.example.jccl_network_project.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Profil_fragment#newInstance} factory method to
+ * Use the {@link A_propos#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Profil_fragment extends Fragment {
+public class A_propos extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-
-
-
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public Profil_fragment() {
+    public A_propos() {
         // Required empty public constructor
     }
 
@@ -40,11 +36,11 @@ public class Profil_fragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment profil_fragment.
+     * @return A new instance of fragment A_propos.
      */
     // TODO: Rename and change types and number of parameters
-    public static Profil_fragment newInstance(String param1, String param2) {
-        Profil_fragment fragment = new Profil_fragment();
+    public static A_propos newInstance(String param1, String param2) {
+        A_propos fragment = new A_propos();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -55,9 +51,6 @@ public class Profil_fragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -68,6 +61,6 @@ public class Profil_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profil_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_a_propos, container, false);
     }
 }
