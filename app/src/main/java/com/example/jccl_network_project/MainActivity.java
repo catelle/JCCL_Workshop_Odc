4b8bc1d9f9aa95a5c928e44764748b060fce187c
Christian K. Djimeli <djkachempire@gmail.com>
2021-10-07 13:16:39 +0100

    Realisation du slide home page

--- app/src/main/java/com/example/jccl_network_project/MainActivity.java
+++ app/src/main/java/com/example/jccl_network_project/MainActivity.java
@@ -7,9 +7,13 @@ import androidx.fragment.app.FragmentContainer;
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TableLayout;
+import android.widget.TextView;
 
 import com.example.jccl_network_project.adapters.Tab_fragment_Adapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -17,6 +21,14 @@ import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int mCpt;
+    private ImageView mButtonToNext;
+    private ImageView mButtonToPrevouis;
+    private ImageView mImageBrand;
+    private TextView mBulletBrandThree;
+    private TextView mBulletBrandOne;
+    private TextView mBulletBrandTwo;
+    private TextView mTextMarketBrand;
     //********** Declaration des variables *************//
     private BottomNavigationView bottomNavigation;
     @Override
@@ -27,6 +39,14 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.setOnNavigationItemSelectedListener( navigationItemSelectedListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container , new Home_fragment()).commit();
+
+        mButtonToNext = findViewById(R.id.next_to_img);
+        mButtonToPrevouis = findViewById(R.id.back_to_img);
+        mImageBrand = findViewById(R.id.img_brand_home);
+        mBulletBrandThree = findViewById(R.id.bullet_3);
+        mBulletBrandTwo = findViewById(R.id.bullet_2);
+        mBulletBrandOne = findViewById(R.id.bullet_1);
+        mTextMarketBrand = findViewById(R.id.text_brand);
 
     }
 
@@ -58,4 +78,53 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    // Gestion des clicks sur la bannière home page
+
+    @SuppressLint("ResourceAsColor")
+    public void nextImage(View view) {
+        mCpt++;
+        if (mCpt == 1){
+            mImageBrand.setImageResource(R.mipmap.ic_commaunity);
+            mBulletBrandOne.setBackgroundColor(R.color.white);
+            mBulletBrandTwo.setBackgroundColor(R.color.orange);
+            mTextMarketBrand.setText(R.string.text_market_forum);
+        }else if(mCpt == 2){
+            mImageBrand.setImageResource(R.mipmap.ic_book);
+            mBulletBrandTwo.setBackgroundColor(R.color.white);
+            mBulletBrandThree.setBackgroundColor(R.color.orange);
+            mTextMarketBrand.setText(R.string.text_market_contenu);
+        }else{
+            mCpt = 0;
+            mImageBrand.setImageResource(R.mipmap.ic_office_work);
+            mTextMarketBrand.setText(R.string.text_market_encardreur);
+            mBulletBrandOne.setBackgroundColor(R.color.orange);
+            mBulletBrandTwo.setBackgroundColor(R.color.white);
+            mBulletBrandThree.setBackgroundColor(R.color.white);
+        }
+
+
+    }
+
+    public void previousImage(View view) {
+
+        if (mCpt == 1){
+            mImageBrand.setImageResource(R.mipmap.ic_office_work);
+            mTextMarketBrand.setText(R.string.text_market_encardreur);
+            mBulletBrandOne.setBackgroundColor(getResources().getColor(R.color.orange));
+            mBulletBrandTwo.setBackgroundColor(getResources().getColor(R.color.white));
+            mBulletBrandThree.setBackgroundColor(getResources().getColor(R.color.white));
+
+        }else if(mCpt == 2){
+            mImageBrand.setImageResource(R.mipmap.ic_commaunity);
+            mBulletBrandOne.setBackgroundColor(getResources().getColor(R.color.white));
+            mBulletBrandTwo.setBackgroundColor(getResources().getColor(R.color.orange));
+            mTextMarketBrand.setText(R.string.text_market_forum);
+        }else{
+            mImageBrand.setImageResource(R.mipmap.ic_book);
+            mBulletBrandTwo.setBackgroundColor(getResources().getColor(R.color.white));
+            mBulletBrandThree.setBackgroundColor(getResources().getColor(R.color.orange));
+            mTextMarketBrand.setText(R.string.text_market_contenu);
+
+        }
+    }
 }
